3472233dc77d7334ba0afe17bed9310c865fa204
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-07-06 12:31:13 +0530

    An Introduction Demo project using Spring frame work named Introductiontospiring is added to repositary

---
+++ Introductiontospiring/src/main/java/com/sudheer/Introductiontospiring/IntroductiontospiringApplication.java
@@ -0,0 +1,13 @@
+package com.sudheer.Introductiontospiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductiontospiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductiontospiringApplication.class, args);
+	}
+
+}
---
+++ Introductiontospiring/src/test/java/com/sudheer/Introductiontospiring/IntroductiontospiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheer.Introductiontospiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductiontospiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
