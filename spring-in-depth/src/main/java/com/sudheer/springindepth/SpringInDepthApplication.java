6e5c6f9257635939933241c95842ca17a1191977
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-07-06 21:59:55 +0530

    for getting indepth undertstanding spring-in-depth project is added to repo

---
+++ spring-in-depth/src/main/java/com/sudheer/springindepth/SpringInDepthApplication.java
@@ -0,0 +1,13 @@
+package com.sudheer.springindepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInDepthApplication.class, args);
+	}
+
+}
---
+++ spring-in-depth/src/test/java/com/sudheer/springindepth/SpringInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheer.springindepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
