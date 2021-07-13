d8589999dcb1668e7b95a11583e27259b9d84492
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-07-13 18:24:53 +0530

    spring boot in 10 steps a demo project for spring intro is added to repositary

---
+++ SpringBootIn10Steps/src/main/java/com/sudheer/springbootbasics/SpringBootIn10Steps/SpringBootIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.sudheer.springbootbasics.SpringBootIn10Steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIn10StepsApplication.class, args);
+	}
+
+}
---
+++ SpringBootIn10Steps/src/test/java/com/sudheer/springbootbasics/SpringBootIn10Steps/SpringBootIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheer.springbootbasics.SpringBootIn10Steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
