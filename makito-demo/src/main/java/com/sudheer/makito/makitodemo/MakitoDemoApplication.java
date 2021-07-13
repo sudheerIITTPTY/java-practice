32e68784dd7be7d76ee5af4b0209852c4d3d5683
sudheerIITTPTY <cs19m005@iittp.ac.in>
2021-07-13 11:38:05 +0530

    makito-demo project is added to learn makito unit testing in spring boot application

---
+++ makito-demo/src/main/java/com/sudheer/makito/makitodemo/MakitoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sudheer.makito.makitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakitoDemoApplication.class, args);
+	}
+
+}
---
+++ makito-demo/src/test/java/com/sudheer/makito/makitodemo/MakitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheer.makito.makitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
